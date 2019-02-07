bc385955c5288389aa0276ba9ec939e2b576cc9b
Michael Rigney <mrigs@umich.edu>
2019-02-07 10:33:17 -0500

    update

--- script.js
+++ script.js
@@ -6,32 +6,13 @@ $(function () {
   });
 });
 
-document.getElementById('scrollTo').scrollIntoView({block: 'start', behavior: 'smooth'});
 
-// $(function() {
-//   $('a[href*=#]').on('click', function(e) {
-//     e.preventDefault();
-//     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 2000, 'linear');
-//   });
-// });
+// document.getElementById('scroll').scrollIntoView({block: 'start', behavior: 'smooth'});
 
-// $(function() {
-//     $('a[href*=#]:not([href=#])').click(function() {
-//         var target = $(this.hash);
-//         target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
-//         if (target.length) {
-//             $('html,body').animate({
-//               scrollTop: target.offset().top
-//             }, 1000);
-//             return false;
-//         }
-//     });
-// });
-// console.log("Hi Hana")
-// $('a[href*="#"]:not([href="#"])').click(function(event){
-// 	console.log("Hi Hana")
-//     $('html, body').animate({
-//         scrollTop: $( $.attr(this, 'href') ).offset().top
-//     }, 1000, "ease-in-out");
-//     event.preventDefault();
-// });
+// $('#scroll').click(
+// 	function (e) {
+// 		console.log("Hi")
+// 		$('html, body').animate({scrollTop: $elem.height()}, 800;
+// 	}
+// );
+  
