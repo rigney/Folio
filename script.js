5ac15de097ed8e5d6116a614174ebffdff8dbf81
Michael Rigney <mrigs@umich.edu>
2019-03-23 21:44:36 -0400

    yuh

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // scrolling to work section from page other than home
 $(function() {
   var hash= window.location.hash
-  if ( hash == '' || hash == '#' || hash == undefined ) return false;
+  // if ( hash == '' || hash == '#' || hash == undefined ) return false;
   var target = $(hash);
   target = target.length ? target : $('[name=' + hash.slice(1) +']');
   if (target.length) {
